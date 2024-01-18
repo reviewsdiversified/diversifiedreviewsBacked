ee9e822d874ac737e8af85278176c7e6922d06cc
Rupesh Bisen <rupesh.bisen2022@gmail.com>
2024-01-18 23:44:31 +0530

    remove index.js file old

--- src/aap.js
+++ src/aap.js
@@ -8,17 +8,16 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
 
 //routes import
 import userRouter from './routes/userRoutes.js'
 
-// app.use("/api/user",require("./routes/userRoutes"));
-app.use("/.netlify/functions/api", userRouter)
+app.use("/api", userRouter)
 
-// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/
 
 export { app }
